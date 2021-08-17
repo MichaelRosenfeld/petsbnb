2032d9e04df54ffdc263a4c819a4582c06e030ab
Bernard Bruck <bernard.bruck@gmail.com>
2021-08-17 18:16:56 +0200

    Solve authentication

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,9 +11,13 @@ class PetsController < ApplicationController
 
   def create
     @pet = Pet.new(pet_params)
-    @pet.save
+    @pet.user = current_user
+    if @pet.save!
+      redirect_to pet_path(@pet)
+    else
+      render :new
+    end
 
-    redirect_to pet_path(@pet)
   end
 
   def new

6eddfd92dc0258dc214b2826a7080fc31c816db3
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-08-17 12:06:01 +0200

    add booking model/controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+  get 'bookings/show'
+  get 'bookings/create'
+  get 'bookings/new'
+  get 'bookings/save'
+  get 'bookings/delete'
+  get 'bookings/edit'
+  get 'bookings/update'
   devise_for :users
   root to: 'pages#home'
   resources :pets

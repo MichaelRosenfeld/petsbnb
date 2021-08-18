c1204e87178f0a2890fd8fd48f46edf9959ab06d
Sascha Grimmer <sascha.grimmer@gmail.com>
2021-08-18 12:53:32 +0200

    Commented out changes in Geocoder configure

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,5 @@
-Geocoder.configure(
-  api_key: pk.eyJ1IjoiZGVhcnNhc2NoYSIsImEiOiJja3J5dzQ5MzUxNDVtMzBxanVzYm8yM3M3In0.JW4OelyFEvuqwqCvWIYjYg
-  units: :km
+# Geocoder.configure(
+#   api_key: pk.eyJ1IjoiZGVhcnNhc2NoYSIsImEiOiJja3J5dzQ5MzUxNDVtMzBxanVzYm8yM3M3In0.JW4OelyFEvuqwqCvWIYjYg
+#   units: :km
 
-)
+# )

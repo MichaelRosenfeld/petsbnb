000f215b87d145a2cf4f83692996e5e171aa0109
Sascha Grimmer <sascha.grimmer@gmail.com>
2021-08-19 17:51:40 +0200

    Run on Autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -31,6 +31,7 @@ require("flatpickr/dist/themes/dark.css");
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 flatpickr(".datepicker", {
 
@@ -43,4 +44,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initAutocomplete();
 });
